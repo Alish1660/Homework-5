b75bcb0563b74fff5b80ceadcad3daa6b613bed3
Alish1660 <Sardor9894>
2024-03-22 23:00:37 -1200

    first

---
+++ main.js
@@ -0,0 +1,79 @@
+////////////////////////////////////////////////////////////////////////
+// 1-misol ////////////////////////////////////////////////////////////
+// let arr = [2, 3, 4, 5, 6, 7];
+// let count_even = 0;
+// let count_odd = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     count_even++;
+//   } else {
+//     count_odd++;
+//   }
+// }
+// console.log(`${count_even} ta juft son bor`);
+// console.log(`${count_odd} ta toq son bor`);
+//////////////////////////////////////////////////////////////////////////////
+// 2-misol //////////////////////////////////////////////////////////////////
+// let arr = [2,3,4,5,6,7,]
+// let count_even = 0
+// let count_odd = 0
+// for(let i = 0; i < arr.length; i++){
+//   if( arr[i] % 2 === 0){
+//     count_even+=arr[i]
+//   } else{
+//     count_odd+=arr[i]
+//   }
+// }
+//   let result = Math.abs(count_even - count_odd)
+// console.log(`${count_even} juft sonlar yigindisi`)
+// console.log(`${count_odd} toq sonlar yigindisi`)
+// console.log( ` juft va toq sonlari yigindisi ortasidagi farq ${result} ga teng`)
+////////////////////////////////////////////////////////////////////////////////////////
+// 3-misol /////////////////////////////////////////////////////////////////////////////////
+// let arr = [1,2,3,4,5,6,7,8]
+// function getSum(arr){
+// let total = 0
+//   for( let i =0; i < arr.length; i++){
+//     total += arr[i]
+//   }
+// }
+// getSum(arr)
+/////////////////////////////////////////////////////////////////////////////////////
+// 4-misol /////////////////////////////////////////////////////////////////////
+// let arr = [  `congrats`,`you `, null,  1,4,undefined,`found`,`strings`]
+// function getStrings(){
+//   for(let i = 0; i < arr.length; i++ ){
+//     if((typeof arr[i])===`string`){
+//       console.log(arr[i])
+//     }
+//   }
+// }
+// getStrings(arr)
+//////////////////////////////////////////////////////////////////////////////////////////
+// 5-misol /////////////////////////////////////////////////////////////////////////////
+// let arr = [1,2,3,4,5,6,7]
+// let added_num = 11
+// function addNum(arr,added_num){
+//   if(arr[0] < added_num){
+//     arr.unshift(added_num)
+//   }else{
+//     arr.push(added_num)
+//   }
+// }
+// addNum(arr,added_num)
+///////////////////////////////////////////////////////////////////////////////////////////
+// 6-misol ////////////////////////////////////////////////////////////////////////////////
+// let arr = [1,2,3,4,5,6,7,8,9]
+// function replaceEvens(arr){
+//   let evens = []
+//   let odds = []
+//   for( let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//       evens.push(arr[i])
+//     }else{
+//       odds.push(arr[i])
+//     }
+//   }
+//   return evens
+//  }
+//  replaceEvens(arr)
